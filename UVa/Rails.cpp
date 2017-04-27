fe007a20672759fc42b92781923bd06027a35ffb
DarrenCai <darren.cai@all-dream.com>
2017-04-27 13:22:19 +0800

    铁轨

--- UVa/Rails.cpp
+++ UVa/Rails.cpp
@@ -16,21 +16,25 @@ int main()
         while(cin>>coache && coache) {
             int n = N, front=1;
             queue<int> marshal;
+            stack<int> sequence;
             marshal.push(coache);
             while(--n) {
                 cin >> coache;
                 marshal.push(coache);
             }
             marshal.push(0);
-            while(marshal.front()) {
-                int top0 = marshal.front()+1, top = top0;
-                while(marshal.front() != front && (top-1 == marshal.front())) {
-                    top = marshal.front();
+            sequence.push(0);
+            while(marshal.front() && front<N+2) {
+                if(sequence.top() > marshal.front()) break;
+                else if(sequence.top() == marshal.front()) {
+                    sequence.pop();
                     marshal.pop();
                 }
-                if(marshal.front() != front || top-1 != marshal.front()) break;
-                marshal.pop();
-                front = top0;
+                else {
+                    while(front < marshal.front()) sequence.push(front ++);
+                    marshal.pop();
+                    ++ front;
+                }
             }
             cout << (marshal.front() ? "No" : "Yes") << endl;
         }
