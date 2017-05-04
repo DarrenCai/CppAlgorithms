f15d7099f9f9d247a3f51218dac70c01bd9f53d5
DarrenCai <darren.cai@all-dream.com>
2017-05-04 18:43:00 +0800

    移动盒子

--- UVa/Boxes in a Line.cpp
+++ UVa/Boxes in a Line.cpp
@@ -4,10 +4,13 @@
  */
 
 #include <iostream>
+#include <cstdio>
 
 int main()
 {
     using namespace std;
+    freopen("in.txt", "r", stdin);
+    freopen("ou.txt", "w", stdout);
     int n, m, count=0;
     while(cin >> n >> m) {
         int *next = new int[n+1]; next[n] = 0;
@@ -16,54 +19,66 @@ int main()
             next[i] = i+1;
             prev[i+1] = i;
         }
+        bool invert = false;
         for(int i=0; i<m; ++i) {
             int op, x, y, t;
             cin >> op;
-            if(op != 4) cin >> x >> y;
-            switch(op) {
-                case 1:
-                    if(next[x] != y) {
-                        next[prev[x]] = next[x];
-                        prev[next[x]] = prev[x];
-                        next[prev[y]] = x;
-                        prev[x] = prev[y];
-                        prev[y] = x;
-                        next[x] = y;
-                    }
-                    break;
-                case 2:
-                    if(next[y] != x) {
-                        next[prev[x]] = next[x];
-                        prev[next[x]] = prev[x];
-                        prev[next[y]] = x;
+            if(op == 4) invert = !invert;
+            else {
+                cin >> x >> y;
+                if(invert && op!=3) op = 3-op;
+                switch(op) {
+                    case 1:
+                        if(next[x] != y) {
+                            next[prev[x]] = next[x];
+                            prev[next[x]] = prev[x];
+                            next[prev[y]] = x;
+                            prev[x] = prev[y];
+                            prev[y] = x;
+                            next[x] = y;
+                        }
+                        break;
+                    case 2:
+                        if(next[y] != x) {
+                            next[prev[x]] = next[x];
+                            prev[next[x]] = prev[x];
+                            prev[next[y]] = x;
+                            next[x] = next[y];
+                            next[y] = x;
+                            prev[x] = y;
+                        }
+                        break;
+                    case 3:
+                        t = prev[y];
+                        prev[y] = prev[x];
+                        next[prev[x]] = y;
+                        next[t] = x;
+                        prev[x] = t;
+                        t = next[x];
                         next[x] = next[y];
-                        next[y] = x;
-                        prev[x] = y;
-                    }
-                    break;
-                case 3:
-                    t = prev[y];
-                    prev[y] = prev[x];
-                    next[prev[x]] = y;
-                    next[t] = x;
-                    prev[x] = t;
-                    t = next[x];
-                    next[x] = next[y];
-                    prev[next[y]] = x;
-                    prev[t] = y;
-                    next[y] = t;
-                    break;
-                case 4:
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wempty-body"
-                    for(x=0; next[x]; prev[x]=next[x], x=next[x]); prev[x] = 0;
-                    for(x=0; prev[x]; next[prev[x]]=x, x=prev[x]); next[x] = 0;
-#pragma clang diagnostic pop
+                        prev[next[y]] = x;
+                        prev[t] = y;
+                        next[y] = t;
+                        break;
+//                     case 4:
+// #pragma clang diagnostic push
+// #pragma clang diagnostic ignored "-Wempty-body"
+//                         for(x=0; next[x]; prev[x]=next[x], x=next[x]); prev[x] = 0;     // 其实这里可以写成 prev[x] = next[x]
+//                         for(x=0; prev[x]; next[prev[x]]=x, x=prev[x]); next[0] = x;     // 其实这里可以写成 next[prev[x]] = x
+// #pragma clang diagnostic pop
+                }
             }
         }
-        int sum = 0;
-        for(int i=0,x=0; next[x]; ++i,x=next[x]) if(i&1) sum+=next[x];
+#if invert
+#define link prev
+#else
+#define link next
+#endif
+        unsigned int sum = 0;
+        for(int i=1,x=0; link[x]; ++i,x=link[x])
+            if(i&1) sum+=link[x];
         cout << "Case " << ++count << ": " << sum << endl;
+        delete[] next; delete[] prev;
     }
     return 0;
 }
