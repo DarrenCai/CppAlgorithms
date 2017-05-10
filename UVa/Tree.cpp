0c7b542e24b8c58d3d40a6ed6cb52d1925c52f7b
DarrenCai <darren.cai@all-dream.com>
2017-05-10 18:29:34 +0800

    树

--- UVa/Tree.cpp
+++ UVa/Tree.cpp
@@ -4,31 +4,73 @@
  */
 
 #include <iostream>
-#include <string>
 #include <deque>
-#include <stack>
+#include <cstdio>
+
+using namespace std;
 
 struct node {
     int val;
     node *left, *right;
+    node(int val=0):left(nullptr),right(nullptr){this->val = val;}
 };
+
+node * buildTree(deque<int>& inOrder, deque<int>& postOrder) {
+    if(postOrder.empty())   return nullptr;
+    const int val = postOrder.back();
+    node* t = new node(val);
+    postOrder.pop_back();
+    deque<int> right;
+    while(inOrder.back() != val) {
+        right.push_front(inOrder.back());
+        inOrder.pop_back();
+    }
+    inOrder.pop_back();
+    if(!right.empty())    t->right = buildTree(right, postOrder);
+    if(!inOrder.empty())  t->left = buildTree(inOrder, postOrder);
+    return t;
+}
+
+void traverse(const node* t, int& min, int& leaf, int sum=0) {
+    sum += t->val;
+    if(sum > min) return;
+    if(t->left && t->right) {
+        traverse(t->left, min, leaf, sum);
+        traverse(t->right, min, leaf, sum);
+    }
+    else if(t->left)    traverse(t->left, min, leaf, sum);
+    else if(t->right)   traverse(t->right, min, leaf, sum);
+    else if(sum<min || (sum==min && t->val<leaf)) {
+        min = sum; leaf = t->val;
+    }
+}
+
+void destoryTree(node *t) {
+    if(t && t->left)     destoryTree(t->left);
+    if(t && t->right)    destoryTree(t->right);
+    if(t)   delete t;
+}
 
 int main()
 {
-    using namespace std;
-    string line1, line2;
-    while(getline(cin, line1) && getline(cin, line2)) {
-        deque<int> inOrder; stack<int> postOrder;
-        line1 += ' '; line2 += ' ';
-        int len=line1.length();
-        for(int v=0,i=0; i<len; ++i) {
-            if(line1[i] == ' ') { inOrder.push_back(v); v=0; }
-            else v=10*v + line1[i]-'0';
+    char c; int v, line=0; deque<int> q[2];
+    freopen("in.txt", "r", stdin);
+    freopen("ou.txt", "w", stdout);
+    while(!cin.eof() && (c=cin.get())) {
+        if(c==' ' || c=='\n') {
+            q[line].push_back(v); v=0;
+            if(c == '\n') {
+                line ^= 1;
+                if(line == 0) {
+                    node *t = buildTree(q[0], q[1]);
+                    int min = 100000000, leaf=10000;
+                    traverse(t, min, leaf);
+                    cout << leaf << endl;
+                    destoryTree(t);     // q[0].clear(); q[1].clear();
+                }
+            } 
         }
-        for(int v=0,i=0; i<len; ++i) {
-            if(line2[i] == ' ') { postOrder.push(v); v=0; }
-            else v=10*v + line2[i]-'0';
-        }
+        else v=10*v + c-'0';
     }
     return 0;
 }
