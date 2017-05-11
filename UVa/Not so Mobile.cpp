7a4119b90e6c7acd9500aeed1fe72f33a8787a20
DarrenCai <darren.cai@all-dream.com>
2017-05-11 19:32:38 +0800

    天平

--- UVa/Not so Mobile.cpp
+++ UVa/Not so Mobile.cpp
@@ -19,14 +19,22 @@ node* build() {
     cin >> Wl >> Dl >> Wr >> Dr;
     node *t = new node();
     if(Wl && Wr) {
-        if(Wl*Dl!=Wr*Dr)    return nullptr;
+        if(Wl*Dl!=Wr*Dr)    {delete t; return nullptr;}
         t->w = Wl + Wr; return t;
     }
     if(!Wl)     t->left = build();
     if(!Wr)     t->right = build();
-    if(!t->left || !t->right)   return nullptr;
-    if(t->left->w*Dl != t->right->w*Dr)     return nullptr;
+    if(!t->left || !t->right) {
+        if(t->left)     delete t->left;
+        if(t->right)    delete t->right;
+        delete t; return nullptr;
+    }
+    if(t->left->w*Dl != t->right->w*Dr) {
+        delete t->left; delete t->right;
+        delete t; return nullptr;
+    }
     t->w = t->left->w + t->right->w;
+    delete t->left; delete t->right;
     return t;
 }
 
@@ -36,7 +44,9 @@ int main()
     freopen("ou.txt", "w", stdout);
     int t; cin >> t;
     while(t--) {
-        cout << (build() ? "YES" : "NO") << endl;
+        node *root = build();
+        cout << (root ? "YES" : "NO") << endl;
+        if(root)    delete root;
         if(t)   cout << endl;
     }
     return 0;
