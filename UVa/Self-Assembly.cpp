68ef403279a13464ca95f5d5cf4a2e2fb4d182c9
DarrenCai <darren.cai@all-dream.com>
2017-06-29 20:15:11 +0800

    自组合

---
+++ UVa/Self-Assembly.cpp
@@ -0,0 +1,18 @@
+/**
+ * UVa1572
+ * 自组合
+ */
+
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+
+int main()
+{
+    using namespace std;
+    int n;
+    while(cin >> n){
+        //
+    }
+    return 0;
+}
