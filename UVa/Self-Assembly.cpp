9d9bc3c85a805b37741626641e9a1328afe186b7
DarrenCai <darren.cai@all-dream.com>
2017-07-05 18:47:54 +0800

    自组合

--- UVa/Self-Assembly.cpp
+++ UVa/Self-Assembly.cpp
@@ -7,49 +7,36 @@
 #include <vector>
 #include <unordered_set>
 using namespace std;
+
+struct molecule{ char e[4];
+    bool operator == (const molecule &m) const {return e[0]==m.e[0] && e[1]==m.e[1] && e[2]==m.e[2] && e[3]==m.e[3];}
+    char count(const char& edge) const {char t=0; for(char i=0; i<4; ++i) if(e[i]==edge) ++t; return t;}
+};
+struct mHash { std::size_t operator () (const pair<molecule, char>& me) const { return me.second*7890481 + me.first.e[0]*148877 + me.first.e[1]*2809 + me.first.e[2]*53 + me.first.e[3]; } };
+#define SET unordered_set<pair<molecule, char>, mHash>
+
+bool search(const pair<molecule, char>& me, const vector<molecule>& m, const unordered_set<unsigned short> (&em)[52], SET& visit, SET path){
+    if(path.count(me)) return true;
+    visit.insert(me); path.insert(me); char e = me.second&1 ? me.second+1 : me.second-1;
+    for(unsigned short i: em[e-1]){
+        for(char j=0; j<4; ++j) if(m[i].e[j] && m[i].e[j] != e && search(pair<molecule, char>(m[i], m[i].e[j]), m, em, visit, path)) return true;
+        if(m[i].count(e) > 1 && search(pair<molecule, char>(m[i], e), m, em, visit, path)) return true;
+    }
+    return false;
+}
+
 #include <cstdio>
-
-bool compatible(int a, int b){
-    for(int i=0; i<4; ++i, a>>=6){
-        int e1 = a & 63; if(!e1) continue;
-        for(int j=0; j<4; ++j, b>>=6){
-            int e2 = b & 63; if(!e2) continue;
-            if((e1&32) != (e2&32) && (e1&31) == (e2&31)) return true;
-        }
-    }
-    return false;
-}
-
-bool search(const vector<unordered_set<int> >& a, vector<bool>& visit, int i, unordered_set<int>& path){
-    if(path.count(i)) return true;
-    visit[i] = true; path.insert(i);
-    for(auto e: a[i]){
-        unordered_set<int> p(path);
-        if(search(a, visit, e, p)) return true;
-    }
-    return false;
-}
-
 int main()
 {
     freopen("in.txt", "r", stdin);
     freopen("ou.txt", "w", stdout);
-    int n;
+    unsigned short n;
     while(cin >> n){
-        vector<int> m(n); vector<unordered_set<int> > a(n); vector<bool> visit(n, false);
-        for(int i=0; i<n; ++i){
-            int v = 0;
-            for(int j=0; j<4; ++j){
-                char c0, c1; cin >> c0 >> c1;
-                v = (v<<6) + (c0=='0' ? 0 : c0-'A'+(c1=='+' ? 32:0));
-            }
-            m[i] = v;
-        }
-        for(int i=0; i<n; ++i) for(int j=0; j<n; ++j) if(!a[i].count(j) && compatible(m[i], m[j])) a[i].insert(j), a[j].insert(i);
+        vector<molecule> m(n); unordered_set<unsigned short> em[52]; SET visit;
+        for(unsigned short i=0; i<n; ++i) for(char j=0; j<4; ++j){char c0, c1; cin >> c0 >> c1; m[i].e[j] = c0=='0' ? 0 : 2*(c0-'A')+(c1=='+' ? 2:1); if(m[i].e[j]) em[m[i].e[j]-1].insert(i);}
         bool bounded = true;
-        for(int i=0; i<n; ++i){
-            unordered_set<int> path;
-            if(!visit[i] && search(a, visit, i, path)){ bounded = false; break; }
+        for(unsigned short i=0; i<n; ++i) for(char j=0; j<4; ++j){ if(!m[i].e[j]) continue; pair<molecule, char> me(m[i], m[i].e[j]);
+            if(!visit.count(me) && search(me, m, em, visit, SET())){bounded = false; break;}
         }
         cout << (bounded ? "bounded" : "unbounded") << endl;
     }
