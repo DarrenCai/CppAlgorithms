83f3813fa9af6d3e7c9f85901ece2e3fb69082fa
DarrenCai <darren.cai@all-dream.com>
2017-07-17 13:07:41 +0800

    系统依赖

--- UVa/System Dependencies.cpp
+++ UVa/System Dependencies.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 #include <map>
 #include <set>
-#include <cstdio>
+// #include <cstdio>
 using namespace std;
 
 int find(vector<string>& ins, string s){
@@ -17,41 +17,41 @@ int find(vector<string>& ins, string s){
     return -1;
 }
 
-void install(string s, map<string, set<string> >& dep, vector<string>& ins){
-    if(dep.count(s) && !dep[s].empty()) for(set<string>::iterator it=dep[s].begin(); it!=dep[s].end(); ++it)
-            if(find(ins, *it) == -1) install(*it, dep, ins);
+void install(string s, map<string, vector<string> >& dep, vector<string>& ins){
+    if(dep.count(s) && !dep[s].empty()) for(int i=0,l=dep[s].size(); i<l; ++i)
+            if(find(ins, dep[s][i]) == -1) install(dep[s][i], dep, ins);
     ins.push_back(s); cout << "   Installing " << s << endl;
 }
 
-void remove(int i, string s, map<string, set<string> >& dep, map<string, set<string> >& r, vector<string>& ins){
+void remove(int i, string s, map<string, vector<string> >& dep, map<string, set<string> >& r, vector<string>& ins, set<string>& ex){
     if(r.count(s) && !r[s].empty()) for(set<string>::iterator it=r[s].begin(); it!=r[s].end(); ++it)
             if(find(ins, *it) > -1) return;
-    ins.erase(ins.begin()+i); cout << "   Removing " << s << endl; int j;
-    if(dep.count(s) && !dep[s].empty()) for(set<string>::iterator it=dep[s].begin(); it!=dep[s].end(); ++it)
-            if((j = find(ins, *it)) > -1) remove(j, *it, dep, r, ins);
+    ins.erase(ins.begin()+i); cout << "   Removing " << s << endl;
+    if(dep.count(s) && !dep[s].empty()) for(int j=0,l=dep[s].size(); j<l; ++j)
+            if(!ex.count(dep[s][j]) && (i = find(ins, dep[s][j])) > -1) remove(i, dep[s][j], dep, r, ins, ex);
 }
 
-void remove(string s, int i, map<string, set<string> >& dep, map<string, set<string> >& r, vector<string>& ins){
+void remove(string s, int i, map<string, vector<string> >& dep, map<string, set<string> >& r, vector<string>& ins, set<string>& ex){
     if(r.count(s) && !r[s].empty()) for(set<string>::iterator it=r[s].begin(); it!=r[s].end(); ++it)
             if(find(ins, *it) > -1) return void(cout << "   " << s << " is still needed." << endl);
-    ins.erase(ins.begin()+i); cout << "   Removing " << s << endl; int j;
-    if(dep.count(s) && !dep[s].empty()) for(set<string>::iterator it=dep[s].begin(); it!=dep[s].end(); ++it)
-            if((j = find(ins, *it)) > -1) remove(j, *it, dep, r, ins);
+    ins.erase(ins.begin()+i); cout << "   Removing " << s << endl;
+    if(dep.count(s) && !dep[s].empty()) for(int j=0,l=dep[s].size(); j<l; ++j)
+            if(!ex.count(dep[s][j]) && (i = find(ins, dep[s][j])) > -1) remove(i, dep[s][j], dep, r, ins, ex);
 }
 
 int main()
 {
-    freopen("in.txt", "r", stdin);
-    freopen("ou.txt", "w", stdout);
-    map<string, set<string> > dep, r; vector<string> ins; stringstream ss; string cmd;
+    // freopen("in.txt", "r", stdin);
+    // freopen("ou.txt", "w", stdout);
+    map<string, vector<string> > dep; map<string, set<string> > r; vector<string> ins; set<string> ex; stringstream ss; string cmd;
     while(getline(cin, cmd) && cmd!="END"){ cout << cmd << endl; ss.str(cmd); ss.clear(); ss >> cmd;
         if(cmd == "DEPEND"){ string s; ss >> s;
-            while(ss >> cmd) dep[s].insert(cmd), r[cmd].insert(s);
+            while(ss >> cmd) dep[s].push_back(cmd), r[cmd].insert(s);
         }else if(cmd == "INSTALL"){ string s; ss >> s;
-            if(find(ins,s) == -1) install(s, dep, ins);
+            if(find(ins,s) == -1) install(s, dep, ins), ex.insert(s);
             else cout << "   " << s << " is already installed." << endl;
         }else if(cmd == "REMOVE"){ string s; ss >> s; int i;
-            if((i = find(ins,s)) > -1) remove(s, i, dep, r, ins);
+            if((i = find(ins,s)) > -1) remove(s, i, dep, r, ins, ex), ex.erase(s);
             else cout << "   " << s << " is not installed." << endl;
         }else{ const int l = ins.size();
             for(int i=0; i<l; ++i) cout << "   " << ins[i] << endl;
