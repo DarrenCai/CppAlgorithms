a066bc375eaf12ad09d1309acfcab910ac5feff7
DarrenCai <darren.cai@all-dream.com>
2017-07-19 18:17:05 +0800

    巡逻机器人

---
+++ UVa/Patrol Robot.cpp
@@ -0,0 +1,21 @@
+/**
+ * UVa1600
+ * 巡逻机器人
+ */
+
+#include <iostream>
+using namespace std;
+char s[20][20]; short m, n, k, mi, ki;
+
+void dfs(){
+    //
+}
+
+int main()
+{
+    short t; cin >> t;
+    while(t--){ mi=200; ki=0; cin >> m >> n >> k;
+        for(char i=0; i<m; ++i) for(int j=0; j<n; ++j) cin >> s[i][j];
+    }
+    return 0;
+}
