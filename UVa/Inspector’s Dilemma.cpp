3e60f21011b38ae1e7ed4b0322df38e741997b68
惆怅客 <lcai13@fudan.edu.cn>
2017-12-18 11:20:27 +0800

    检查员的难题

--- UVa/Inspector’s Dilemma.cpp
+++ UVa/Inspector’s Dilemma.cpp
@@ -5,54 +5,56 @@
 
 #include <iostream>
 #include <vector>
-#include <set>
+// #include <cstdio>
+using namespace std;
+
+vector<short> g[1001];
+bool visit[1001], calc[1001];
+short d[1001];
+
+int dfs(short i) {
+    int t = 0;
+    visit[i] = true;
+    if (!calc[i]) {
+        if (d[i] & 1) ++t;
+        calc[i] = true;
+    }
+    short size = g[i].size();
+    for (short j=0; j<size; ++j) {
+        if (!calc[g[i][j]]) {
+            if (d[g[i][j]] & 1) ++t;
+            calc[g[i][j]] = true;
+        }
+        if (!visit[g[i][j]]) t += dfs(g[i][j]);
+    }
+    return t;
+}
 
 int main()
 {
-    using namespace std;
+    // freopen("in.txt", "r", stdin);
+    // freopen("ou.txt", "w", stdout);
     short v, t; int e, k=0;
-    while(cin >> v >> e >> t && v != 0) {
-        vector<int> es(e); short d[1001]={0};
+    while (cin >> v >> e >> t && v) {
+        for (short i=1; i<=v; ++i) {
+            visit[i] = calc[i] = false; d[i] = 0;
+        }
         for (int i=0; i<e; ++i) {
             short a, b; cin >> a >> b;
+            g[a].push_back(b);
+            g[b].push_back(a);
             ++ d[a]; ++ d[b];
-            es[i] = a<<10|b;
         }
-        vector<set<short> > bs; short c[500]={0}, s=0, r=0;
-        for (int i=0; i<es.size(); ++i) {
-            short a = es[i]>>10, b = es[i]&1023;
-            bool contain = false;
-            for (short j=0; j<s; ++j) {
-                if (bs[j].count(a) || bs[j].count(b)) {
-                    bs[j].insert(a); bs[j].insert(b);
-                    ++c[j]; contain = true; break;
-                }
-            }
-            if(!contain) {
-                bs.push_back(set<short>()); ++c[s];
-                bs[s].insert(a); bs[s++].insert(b);
-            }
-            for(vector<int>::iterator it = es.begin()+i+1; it != es.end();) {
-                a = *it>>10, b = *it&1023;
-                bool contain = false;
-                for (short j=0; j<s; ++j) {
-                    if (bs[j].count(a) || bs[j].count(b)) {
-                        bs[j].insert(a); bs[j].insert(b);
-                        ++c[j]; it = es.erase(it);
-                        contain = true; break;
-                    }
-                }
-                if(!contain) ++it;
+        int r = 0;
+        for (short i=1; i<=v; ++i) {
+            if (!visit[i] && g[i].size()) {
+                int s = dfs(i);
+                r += s ? s : 2;
             }
         }
-        for (short i=0; i<s; ++i) {
-            const set<short>& b = bs[i]; short odd = 0;
-            for (set<short>::iterator it = b.begin(); it != b.end(); ++it)
-                if (d[*it] & 1) ++odd;
-            r += c[i] + (odd > 2 ? (odd-2)>>1 : 0);
-        }
-        if (s > 1) r += s-1;
-        cout << "Case " << ++k << ": " << r*t << endl;
+        if (r) r = (r-2) >> 1;
+        cout << "Case " << ++k << ": " << (e+r)*t << endl;
+        for (short i=1; i<=v; ++i) g[i].clear();
     }
     return 0;
 }
