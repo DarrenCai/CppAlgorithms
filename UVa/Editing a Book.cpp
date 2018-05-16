72fc58f15d76f257f5ac74f37e004a4c9829c62c
惆怅客 <lcai13@fudan.edu.cn>
2018-05-17 01:31:32 +0800

    编辑书稿,IDA*解法优化。 策略1：假设剪切片段的第一个数字为a，最后一个数字为b，要么把这个片段粘贴到a-1的下一个位置，要么粘贴到b+1的前一个位置。 策略2：永远不要“破坏”一个已经连续排列的数字片段。例如，不能把1 2 3 4中的2 3剪切出来。

--- UVa/Editing a Book.cpp
+++ UVa/Editing a Book.cpp
@@ -7,6 +7,14 @@
 
 short n; char p[9], ops;
 
+char idx(const char *s, char a) {
+    for (char i=0; i<n; ++i)
+        if (s[i] == a) return i;
+    return 0;
+}
+
+bool expand(const char *s, char d, char i, char l, char j);
+
 bool IDAStar(const char* s, char d=0) {
     bool isAns = true;
     for (char i=0; isAns && i<n; ++i)
@@ -17,23 +25,32 @@ bool IDAStar(const char* s, char d=0) {
         for (char i=0; i<n-1; ++i) {
             for (char l=1; l<n-i; ++l) {
                 if ((!i || s[i] != s[i-1] + 1) && s[i+l] != s[i+l-1] + 1) {
-                    for (char j=i+l; j<n; ++j) {
-                        if (j==n-1 || s[j+1] != s[j] + 1) {
-                            char s1[9], t=0;
-                            for (char k=0; k<i; ++k) s1[k] = s[k];
-                            for (char k=i+l; k<=j; ++k) s1[k-l] = s[k];
-                            for (char k=i; k<i+l; ++k) s1[j+1+k-i-l] = s[k];
-                            for (char k=j+1; k<n; ++k) s1[k] = s[k];
-                            for (char k=0; k<n-1; ++k)
-                                if (s1[k]+1 != s1[k+1]) ++t;
-                            if (3*(d+1) + t <= 3*ops && IDAStar(s1, d+1))
+                    char a = idx(s, s[i]-1), b = idx(s, s[i+l-1]+1)-1;
+                    if (a<i+l && b<i+l) {
+                        for (char j=i+l; j<n; ++j)
+                            if ((j==n-1 || s[j+1] != s[j] + 1) && expand(s, d+1, i, l, j))
                                 return true;
-                        }
+                    } else {
+                        if (a>=i+l && expand(s, d+1, i, l, a)) return true;
+                        if (b>=i+l && expand(s, d+1, i, l, b)) return true;
                     }
                 }
             }
         }
     }
+    return false;
+}
+
+bool expand(const char *s, char d, char i, char l, char j) {
+    char s1[9], t=0;
+    for (char k=0; k<i; ++k) s1[k] = s[k];
+    for (char k=i+l; k<=j; ++k) s1[k-l] = s[k];
+    for (char k=i; k<i+l; ++k) s1[j+1+k-i-l] = s[k];
+    for (char k=j+1; k<n; ++k) s1[k] = s[k];
+    for (char k=0; k<n-1; ++k)
+        if (s1[k]+1 != s1[k+1]) ++t;
+    if (3*d + t <= 3*ops && IDAStar(s1, d))
+        return true;
     return false;
 }
 
