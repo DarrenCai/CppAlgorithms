77e4c82f078ce918828dd3d671cad30780c74184
惆怅客 <lcai13@fudan.edu.cn>
2018-05-17 00:49:48 +0800

    编辑书稿,IDA*解法

---
+++ UVa/Editing a Book.cpp
@@ -0,0 +1,52 @@
+/**
+ * UVa11212
+ * 编辑书稿
+ */
+
+#include <iostream>
+
+short n; char p[9], ops;
+
+bool IDAStar(const char* s, char d=0) {
+    bool isAns = true;
+    for (char i=0; isAns && i<n; ++i)
+        if (s[i] != '1'+i) isAns = false;
+    if (isAns) {
+        return true;
+    } else if (d < ops) {
+        for (char i=0; i<n-1; ++i) {
+            for (char l=1; l<n-i; ++l) {
+                if ((!i || s[i] != s[i-1] + 1) && s[i+l] != s[i+l-1] + 1) {
+                    for (char j=i+l; j<n; ++j) {
+                        if (j==n-1 || s[j+1] != s[j] + 1) {
+                            char s1[9], t=0;
+                            for (char k=0; k<i; ++k) s1[k] = s[k];
+                            for (char k=i+l; k<=j; ++k) s1[k-l] = s[k];
+                            for (char k=i; k<i+l; ++k) s1[j+1+k-i-l] = s[k];
+                            for (char k=j+1; k<n; ++k) s1[k] = s[k];
+                            for (char k=0; k<n-1; ++k)
+                                if (s1[k]+1 != s1[k+1]) ++t;
+                            if (3*(d+1) + t <= 3*ops && IDAStar(s1, d+1))
+                                return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    using namespace std;
+    short k=0;
+    while (cin>>n && n) {
+        for (short i=0; i<n; ++i)
+            cin >> p[i];
+        ops = 0;
+        while (!IDAStar(p)) ++ ops;
+        cout << "Case " << ++k << ": " << short(ops) << endl;
+    }
+    return 0;
+}
