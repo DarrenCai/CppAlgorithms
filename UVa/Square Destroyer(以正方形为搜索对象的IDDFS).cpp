db48bbdf4f07041fb102b0a1d5bf59204ff06cb0
惆怅客 <lcai13@fudan.edu.cn>
2018-06-26 02:36:02 +0800

    破坏正方形

---
+++ UVa/Square Destroyer(以正方形为搜索对象的IDDFS).cpp
@@ -0,0 +1,114 @@
+/**
+ * UVa1603
+ * 破坏正方形
+ */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+short n, k, depth; char e[60], size[5][5][6], maxMatch, offset;
+
+void takeH(char x, char y, char l) {
+    for (char ll=l; ll<=x; ++ll) {
+        char xx = x-ll;
+        for (short i = max(y+1-ll, 0); i<=y && i<=n-ll; ++i)
+            -- size[xx][i][ll];
+    }
+    for (char ll=l; ll<=n-x; ++ll)
+        for (short i = max(y+1-ll, 0); i<=y && i<=n-ll; ++i)
+            -- size[x][i][ll];
+}
+
+void putH(char x, char y, char l) {
+    for (char ll=l; ll<=x; ++ll) {
+        char xx = x-ll;
+        for (short i = max(y+1-ll, 0); i<=y && i<=n-ll; ++i)
+            ++ size[xx][i][ll];
+    }
+    for (char ll=l; ll<=n-x; ++ll)
+        for (short i = max(y+1-ll, 0); i<=y && i<=n-ll; ++i)
+            ++ size[x][i][ll];
+}
+
+void takeV(char x, char y, char l) {
+    for (char ll=l; ll<=y; ++ll) {
+        char yy = y - ll;
+        for (char i = max(x+1-ll, 0); i<=x && i<=n-ll; ++i)
+            -- size[i][yy][ll];
+    }
+    for (char ll=l; ll<=n-y; ++ll)
+        for (short i = max(x+1-ll, 0); i<=x && i<=n-ll; ++i)
+            -- size[i][y][ll];
+}
+
+void putV(char x, char y, char l) {
+    for (char ll=l; ll<=y; ++ll) {
+        char yy = y - ll;
+        for (char i = max(x+1-ll, 0); i<=x && i<=n-ll; ++i)
+            ++ size[i][yy][ll];
+    }
+    for (char ll=l; ll<=n-y; ++ll)
+        for (short i = max(x+1-ll, 0); i<=x && i<=n-ll; ++i)
+            ++ size[i][y][ll];
+}
+
+bool IDDFS(char x=0, char y=0, char l=1, char step = 0) {
+    if (y+l > n) { y = 0; ++x; }
+    if (x+l > n) { x = 0; ++l; }
+    if (l > n) return true;
+    if (size[x][y][l] < l<<2) {
+        return IDDFS(x, y+1, l, step);
+    } else {
+        for (char i=0; i<l; ++i) {
+            char e1 = x*offset+y+i, e2 = (x+l)*offset+y+i,
+                 e3 = (x+i)*offset+n+y, e4 = (x+i)*offset+n+y+l;
+            if (e[e1]) {
+                e[e1] = 0; takeH(x, y+i, l);
+                if (step+1 <= depth && IDDFS(x, y+1, l, step+1)) return true;
+                e[e1] = 1; putH(x, y+i, l);
+            }
+            if (e[e2]) {
+                e[e2] = 0; takeH(x+l, y+i, l);
+                if (step+1 <= depth && IDDFS(x, y+1, l, step+1)) return true;
+                e[e2] = 1; putH(x+l, y+i, l);
+            }
+            if (e[e3]) {
+                e[e3] = 0; takeV(x+i, y, l);
+                if (step+1 <= depth && IDDFS(x, y+1, l, step+1)) return true;
+                e[e3] = 1; putV(x+i, y, l);
+            }
+            if (e[e4]) {
+                e[e4] = 0; takeV(x+i, y+l, l);
+                if (step+1 <= depth && IDDFS(x, y+1, l, step+1)) return true;
+                e[e4] = 1; putV(x+i, y+l, l);
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int t; cin >> t;
+    while (t--) {
+        cin >> n >> k; char c = 0;
+        maxMatch = n*(n+1)<<1;
+        offset = 1+(n<<1);
+        memset(e, 1, maxMatch);
+        for (short i=0; i<k; ++i) {
+            short j; cin >> j; --j; e[j] = 0;
+        }
+        for (char l=1; l<=n; ++l)
+            for (char x=n-l; x>=0; --x)
+                for (char y=n-l; y>=0; --y) {
+                    size[x][y][l] = 0;
+                    for (char i=0; i<l; ++i) 
+                        size[x][y][l] += 
+                            e[x*offset+y+i] + e[(x+l)*offset+y+i] + e[(x+i)*offset+n+y] + e[(x+i)*offset+n+y+l];
+                }
+        for (depth=0; !IDDFS(); ++depth);
+        cout << depth << endl;
+    }
+    return 0;
+}
---
+++ UVa/Square Destroyer(以火柴为搜索对象的IDDFS).cpp
@@ -0,0 +1,119 @@
+/**
+ * UVa1603
+ * 破坏正方形
+ */
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+short n, k, depth; char e[60], d[60], size[5][5][6], maxMatch, offset;
+bool cmp(char a, char b) {return d[a] < d[b];}
+
+void take(char x, char y, char l, char& cnt) {
+    if (size[x][y][l] == l<<2) {
+        -- cnt;
+        for (char i=0; i<l; ++i) {
+            char e1 = x*offset+y+i, e2 = (x+l)*offset+y+i,
+                 e3 = (x+i)*offset+n+y, e4 = (x+i)*offset+n+y+l;
+            -- d[e1]; --d[e2]; --d[e3]; --d[e4];
+        }
+    }
+}
+
+void put(char x, char y, char l, char& cnt) {
+    if (size[x][y][l] == l<<2) {
+        ++ cnt;
+        for (char i=0; i<l; ++i) {
+            char e1 = x*offset+y+i, e2 = (x+l)*offset+y+i,
+                 e3 = (x+i)*offset+n+y, e4 = (x+i)*offset+n+y+l;
+            ++ d[e1]; ++d[e2]; ++d[e3]; ++d[e4];
+        }
+    }
+}
+
+bool IDDFS(char cnt, vector<char> s, char step = 0) {
+    if (cnt == 0) return true;
+    for (char p = s.size()-1; p>=0; --p) {
+        char edge = s[p], t = 1, sum = d[edge];
+        while (t<=p && sum<cnt) sum += d[s[p-t++]];
+        if (step+t > depth || sum<cnt) return false;
+        vector<char> ss(s.begin(), s.begin()+p);
+        char x = edge/(2*n+1), y = edge%(2*n+1);
+        if (y < n) {
+            for (char l=1; l<=x; ++l) {
+                char i = x-l;
+                for (short j = max(y+1-l, 0); j<=y && j<=n-l; ++j)
+                    take(i, j, l, cnt), -- size[i][j][l];
+            }
+            for (char l=1; l<=n-x; ++l)
+                for (char j = max(y+1-l, 0); j<=y && j<=n-l; ++j)
+                    take(x, j, l, cnt), -- size[x][j][l];
+            if (cnt == 0) return true;
+            sort(ss.begin(), ss.end(), cmp);
+            if (step+1 < depth && IDDFS(cnt, ss, step+1)) return true;
+            for (char l=1; l<=x; ++l) {
+                char i = x-l;
+                for (short j = max(y+1-l, 0); j<=y && j<=n-l; ++j)
+                    ++ size[i][j][l], put(i, j, l, cnt);
+            }
+            for (char l=1; l<=n-x; ++l)
+                for (char j = max(y+1-l, 0); j<=y && j<=n-l; ++j)
+                    ++ size[x][j][l], put(x, j, l, cnt);
+        } else {
+            y -= n;
+            for (char l=1; l<=y; ++l) {
+                char j = y - l;
+                for (char i = max(x+1-l, 0); i<=x && i<=n-l; ++i)
+                    take(i, j, l, cnt), -- size[i][j][l];
+            }
+            for (char l=1; l<=n-y; ++l)
+                for (char i = max(x+1-l, 0); i<=x && i<=n-l; ++i)
+                    take(i, y, l, cnt), -- size[i][y][l];
+            if (cnt == 0) return true;
+            sort(ss.begin(), ss.end(), cmp);
+            if (step+1 < depth && IDDFS(cnt, ss, step+1)) return true;
+            for (char l=1; l<=y; ++l) {
+                char j = y - l;
+                for (char i = max(x+1-l, 0); i<=x && i<=n-l; ++i)
+                    ++ size[i][j][l], put(i, j, l, cnt);
+            }
+            for (char l=1; l<=n-y; ++l)
+                for (char i = max(x+1-l, 0); i<=x && i<=n-l; ++i)
+                    ++ size[i][y][l], put(i, y, l, cnt);
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int t; cin >> t;
+    while (t--) {
+        cin >> n >> k; char cnt = 0;
+        maxMatch = n*(n+1)<<1;
+        offset = 1+(n<<1);
+        memset(e, 1, maxMatch);
+        memset(d, 0, maxMatch);
+        for (char i=0; i<k; ++i) {
+            short j; cin >> j; --j; e[j] = 0;
+        }
+        for (char l=1; l<=n; ++l)
+            for (char x=n-l; x>=0; --x)
+                for (char y=n-l; y>=0; --y) {
+                    size[x][y][l] = 0;
+                    for (char i=0; i<l; ++i)
+                        size[x][y][l] += 
+                            e[x*offset+y+i] + e[(x+l)*offset+y+i] + e[(x+i)*offset+n+y] + e[(x+i)*offset+n+y+l];
+                    put(x, y, l, cnt);
+                }
+        vector<char> s;
+        for (char i=0; i<maxMatch; ++i) if (d[i]) s.push_back(i);
+        sort(s.begin(), s.end(), cmp);
+        for (depth=0; !IDDFS(cnt, s); ++depth);
+        cout << depth << endl;
+    }
+    return 0;
+}
