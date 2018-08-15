f4a5a3844baec74ee7fd0941c2228faa4a1ff981
惆怅客 <lcai13@fudan.edu.cn>
2018-08-15 12:18:49 +0800

    树上的机器人规划（简单版）

---
+++ UVa/Planning mobile robot on Tree(EASY Version).cpp
@@ -0,0 +1,63 @@
+/**
+ * UVa12569
+ * 树上的机器人规划（简单版）
+ */
+
+#include <iostream>
+#include <queue>
+#include <map>
+#include <cstring>
+using namespace std;
+
+int n, m, s, t, T, obs[15], c; bool g[16][16]; map<int, int> p;
+
+int bfs() {
+    p.clear(); p[c] = 0; queue<int> q; q.push(c); int step = 0;
+    while (!q.empty()) {
+        for (int i=q.size(); i; --i) {
+            c = q.front(); q.pop(); s = c>>16;
+            if (s == t) return step;
+            if (g[s][t] && !(c&1<<t)) {
+                int cc = (c&0xffff & ~(1<<s)) | 1<<t | t<<16;
+                p[cc] = c; c = cc;
+                return step+1;
+            }
+            for (int u=1; u<=n; ++u)
+                if (c&1<<u)
+                    for (int v=1; v<=n; ++v)
+                        if (g[u][v] && !(c&1<<v)) {
+                            int cc = (c&0xffff & ~(1<<u)) | 1<<v | (u==s ? v : s)<<16;
+                            if (!p.count(cc)) p[cc] = c, q.push(cc);
+                        }
+        }
+        ++ step;
+    }
+    return -1;
+}
+
+void printPath(int c) {
+    int pre = p[c];
+    if (pre) {
+        printPath(pre);
+        int u, v;
+        for (int i=1; i<=n; ++i) {
+            if (pre&1<<i && !(c&1<<i)) u = i;
+            if (c&1<<i && !(pre&1<<i)) v = i;
+        }
+        cout << u << ' ' << v << endl;
+    }
+}
+
+int main()
+{
+    cin >> T;
+    for (int k=1; k<=T; ++k) {
+        memset(g, 0, sizeof(g));
+        cin >> n >> m >> s >> t; c = 1<<s | s<<16;
+        for (int i=1; i<=m; ++i) cin >> s, c |= 1<<s;
+        for (int i=1; i<n; ++i) { int u, v; cin >> u >> v; g[u][v] = g[v][u] = true; }
+        int step = bfs(); cout << "Case " << k << ": " << step << endl;
+        if (step != -1) printPath(c); cout << endl;
+    }
+    return 0;
+}
