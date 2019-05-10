71a54da9c0abf7e07a6eae33cd02d1c27b1f9c5a
惆怅客 <lcai13@fudan.edu.cn>
2019-05-10 10:03:48 +0800

    Square，剪枝策略同Sticks，但一定要注意数值范围更大了，原来部分用short的地方要改成int

---
+++ UVa/Square.cpp
@@ -0,0 +1,43 @@
+/**
+ * UVa10364
+ * 正方形
+ */
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+short s[25], n; int ans; bool visit[25];
+
+bool dfs(int r, short k=3, short f=n-1) {
+    if (r == 0) return k==1 || dfs(ans, k-1, n-1);
+    if (!visit[f] && s[f]<=r && !(f+1<n && !visit[f+1] && s[f]==s[f+1])) {
+        visit[f] = true;
+        if (dfs(r-s[f], k, f-1)) return true;
+        visit[f] = false;
+        if (r == ans || r == s[f]) return false;
+    }
+    return f>0 && dfs(r, k, f-1);
+}
+
+int main()
+{
+    freopen("in.txt", "r", stdin);
+    freopen("ou.txt", "w", stdout);
+    int k; cin >> k;
+    while (k--) {
+        int t = 0; memset(visit, 0, 25); cin >> n;
+        for (short i=0; i<n; ++i) {
+            cin >> s[i]; t += s[i];
+        }
+        ans = t >> 2;
+        if (t != ans<<2) cout << "no" << endl;
+        else {
+            sort(s, s+n);
+            if (s[n-1] > ans) cout << "no" << endl;
+            else cout << (dfs(ans) ? "yes" : "no") << endl;
+        }
+    }
+    return 0;
+}
