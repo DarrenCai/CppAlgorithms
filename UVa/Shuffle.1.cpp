c02bbf0236acd64f4338c18e1976aa9b461cd337
惆怅客 <lcai13@fudan.edu.cn>
2019-08-09 14:13:08 +0800

    Shuffle的播放记录,O(n)解法

--- UVa/Shuffle.1.cpp
+++ UVa/Shuffle.1.cpp
@@ -5,7 +5,6 @@
 
 #include <iostream>
 #include <vector>
-#include <set>
 using namespace std;
 
 #define min(a,b) (a<b ? a:b)
@@ -14,30 +13,28 @@ int a[N];
 
 int main()
 {
-    freopen("in.txt", "r", stdin);
-    freopen("ou.txt", "w", stdout);
+    // freopen("in.txt", "r", stdin);
+    // freopen("ou.txt", "w", stdout);
     int d; cin >> d;
     while (d--) {
         int s, n, ans=0; cin >> s >> n;
         for (int i=0; i<n; ++i) cin >> a[i];
-        int t = (n + s-1) / s; vector<set<int> > v1(t); vector<multiset<int> > v2(t);
+        int t = (n + s-1) / s; vector<vector<int> > v1(t, vector<int>(s+1, 0)); vector<int> v2(t, 0);
         for (int i=0; i<n; ++i) {
-            int p = (i+s) / s - 1; v1[p].insert(a[i]); v2[p].insert(a[i]);
+            int p = (i+s) / s - 1; if (++v1[p][a[i]] == 1) ++v2[p];
         }
-        bool used[N] = {false};
+        vector<bool> used(s+1, false);
         for (int i=0, m=min(s,n); i<m; ++i) {
             if (used[a[i]]) break;
             bool ok = true; used[a[i]] = true;
             for (int j=i; j<n; j+=s) {
-                int p = (j+s) / s - 1;
-                if (v2[p].count(a[j]) == 1) v1[p].erase(a[j]);
-                v2[p].erase(v2[p].find(a[j]));
+                int p = (j+s) / s - 1; if (--v1[p][a[j]] == 0) --v2[p];
                 if (p > 0) {
-                    v1[p-1].insert(a[j]); v2[p-1].insert(a[j]);
-                    if (v2[p-1].size() > v1[p-1].size()) ok = false;
+                    if (++v1[p-1][a[j]] == 1) ++v2[p-1];
+                    if (v2[p-1] < s) ok = false;
                 }
             }
-            int p = (n-i-1 + s-1) / s - 1; if (v2[p].size() > v1[p].size()) ok = false;
+            int p = (n-i-1 + s-1) / s - 1; if (v2[p] < (n-i-1)%s) ok = false;
             if (ok) ++ ans;
         }
         if (s>n && ans==n) ans += s-n;
