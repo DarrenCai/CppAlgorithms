7efd5aa337cdcd29b2f12bbad30d27732dba9db4
DarrenCai <lcai13@fudan.edu.cn>
2021-03-17 18:58:39 +0800

    更新一些数学计算的实现

--- template/math.cpp
+++ template/math.cpp
@@ -31,3 +31,30 @@ void gcd(int a, int b, int& g, int& x, int& y) {
         y -= x*(a/b);
     }
 }
+
+/**
+ * 求1, 2, 3, ..., n中与n互素的数的个数
+ */
+int euler_phi(int n) {
+    int m = sqrt(n+0.5);
+    int ans = n;
+    for(int i = 2; i <= m; i++) if(n % i == 0) {
+        ans = ans / i * (i-1);
+        while(n % i == 0) n /= i;
+    }
+    if(n > 1) ans = ans / n * (n-1);
+    return ans;
+}
+
+/**
+ * 计算1~n中所有数的欧拉phi函数值,复杂度O(nlogn)
+ */
+void phi_table(int n, int* phi) {
+    for(int i = 2; i <= n; i++) phi[i] = 0;
+    phi[1] = 1;
+    for(int i = 2; i <= n; i++) if(!phi[i])
+        for(int j = i; j <= n; j += i) {
+            if(!phi[j]) phi[j] = j;
+            phi[j] = phi[j] / i * (i-1);
+        }
+}
