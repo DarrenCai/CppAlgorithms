2b371d7b7a65bb58b6aafd13be537443e2945b39
DarrenCai <lcai13@fudan.edu.cn>
2021-03-25 15:16:18 +0800

    过河

---
+++ UVa/Crossing Rivers.cpp
@@ -0,0 +1,26 @@
+/**
+ * UVa12230
+ * 过河
+ */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // freopen("in.txt", "r", stdin);
+    // freopen("ou.txt", "w", stdout);
+    cout << fixed << setprecision(3);
+    short n, d, kase = 0;
+    while (cin >>n>>d && d) {
+        double t = .0;
+        for (short i=0; i<n; ++i) {
+            short x, l, v; cin >> x >> l >> v;
+            d -= l; t += 2.*l/v;
+        }
+        t += d;
+        cout << "Case " << ++kase << ": " << t << endl << endl;
+    }
+    return 0;
+}
