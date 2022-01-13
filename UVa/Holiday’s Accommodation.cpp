1bdf4b0f3791d62414e83ac9c3046838213d56f1
DarrenCai <lcai13@fudan.edu.cn>
2022-01-13 09:45:59 +0800

    交换房子

--- UVa/Holiday’s Accommodation.cpp
+++ UVa/Holiday’s Accommodation.cpp
@@ -8,14 +8,13 @@
 using namespace std;
 
 #define N 100100
-int u[N], v[N], cu[N], cv[N], n; long long w[N]; vector<int> g[N];
+int u[N], v[N], n; long long w[N], ans; vector<int> g[N];
 
 int dfs(int i, int p = -1) {
-    cu[i] = cv[i] = 0;
-    bool isU = p>0 && (v[i]==u[p] || v[i]==v[p]);
-    int a = isU ? u[i] : v[i], &ref = isU ? cu[i] : cv[i];
-    for (int j=g[a].size()-1, k; j>=0; --j) if ((k=g[a][j]) != i && k != p) ref += 1 + dfs(k, i);
-    return ref;
+    int a = p>0 && (v[i]==u[p] || v[i]==v[p]) ? u[i] : v[i], c = 0;
+    for (int j=g[a].size()-1, k; j>=0; --j) if ((k=g[a][j]) != i && k != p) c += 1 + dfs(k, i);
+    ans += w[i]*min(c+1, n-c-1)<<1;
+    return c;
 }
 
 int main() {
@@ -26,12 +25,7 @@ int main() {
         cin >> n;
         for (int i=1; i<=n; ++i) g[i].clear();
         for (int i=1; i<n; ++i) cin >> u[i] >> v[i] >> w[i], g[u[i]].push_back(i), g[v[i]].push_back(i);
-        dfs(1);
-        long long ans = 0;
-        for (int i=1; i<n; ++i) {
-            cu[i] == 0 ? (++cv[i], cu[i]=n-cv[i]) : (++cu[i], cv[i]=n-cu[i]);
-            ans += w[i]*(min(cu[i], cv[i]))<<1;
-        }
+        ans = 0; dfs(1);
         cout << "Case #" << kase << ": " << ans << endl;
     }
     return 0;
