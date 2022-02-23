87e43be0bc1e292a2c78c90b8f35e120a253dda6
DarrenCai <lcai13@fudan.edu.cn>
2022-02-23 13:43:46 +0800

    ⿊客的攻击,缩短一点运行时间

--- UVa/Hackers’ Crackdown.cpp
+++ UVa/Hackers’ Crackdown.cpp
@@ -14,16 +14,17 @@ int main() {
     // freopen("ou.txt", "w", stdout);
     int kase = c[0] = d[0] = 0;
     while (cin>>n && n) {
-        int a = (1<<n) - 1, m, v;
+        int a = (1<<n) - 1, cc = 0, s = 0, ss, m, v;
         for (int i=0; i<n; ++i) {
-            int &ref = c[1<<i] = 1<<i;
-            cin >> m; while (m--) cin >> v, ref |= 1<<v;
+            ss = 1<<i; cin >> m;
+            while (m--) cin >> v, ss |= 1<<v;
+            ss == a ? ++cc : c[1<<s++] = ss;
         }
-        for (int s=1; s<=a; ++s) {
-            c[s] = c[s&(s-1)] | c[s&-s]; d[s] = c[s] == a;
-            for (int ss=s; ss; ss=s&(ss-1)) d[s] = max(d[s], d[s^ss]+d[ss]);
+        for (ss=(1<<s)-1, s=1; s<=ss; ++s) {
+            c[s] = c[s&(s-1)] | c[s&-s];
+            if (d[s] = c[s] == a) for (m=s&(s-1), v=s>>1; m>v; m=s&(m-1)) d[s] = max(d[s], d[s^m]+d[m]);
         }
-        cout << "Case " << ++kase << ": " << d[a] << endl;
+        cout << "Case " << ++kase << ": " << cc + d[ss] << endl;
     }
     return 0;
 }
