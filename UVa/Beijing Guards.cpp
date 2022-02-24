7be6d6fbd3b1e140c8570ff4f1cda6fe039e1a21
DarrenCai <lcai13@fudan.edu.cn>
2022-02-24 19:15:14 +0800

    长城守卫,不需要数组

--- UVa/Beijing Guards.cpp
+++ UVa/Beijing Guards.cpp
@@ -6,18 +6,15 @@
 #include <iostream>
 using namespace std;
 
-#define N 1000100
-int r[N], n;
-
 int main() {
     // freopen("in.txt", "r", stdin);
     // freopen("ou.txt", "w", stdout);
     ios::sync_with_stdio(false);
-    while (cin>>n && n) {
-        long long s = 0; int m = n>>1;
-        for (int i=0; i<n; ++i) cin >> r[i], s += r[i];
-        int ans = m<1 ? r[0] : max(int((s+m-1)/m), r[n-1]+r[0]);
-        for (int i=1; i<n; ++i) ans = max(ans, r[i-1]+r[i]);
+    int n, r0;
+    while (cin>>n && n && cin>>r0) {
+        long long s = r0; int m = n>>1, p = r0, r, ans = r0;
+        for (int i=1; i<n; ++i) cin >> r, s += r, ans = max(ans, r+p), p = r;
+        if (n > 1) ans = max(ans, max(int((s+m-1)/m), r0+r));
         cout << ans << endl;
     }
     return 0;
