4448e0f92937538381f06e59cf4c24a4025330fe
DarrenCai <lcai13@fudan.edu.cn>
2022-09-15 17:34:28 +0800

    表1-10，UVa1412/LA4048，Fund Management，基金管理

--- UVa/Fund Management.cpp
+++ UVa/Fund Management.cpp
@@ -1,79 +1,70 @@
 /**
- * UVa1412
+ * UVa1412/LA4048
  * 基金管理
+ * NEERC 2007
  */
 
 #include <iostream>
 #include <cstring>
 using namespace std;
 
-#define min(a, b) (a<b ? a:b)
 #define M 102
-#define N 10
-#define MX 13000    // c(16,8) < 13000
+#define N 9
+#define MX 12880    // C(16,8) = 12870
 #define X 38263753  // 8*9^7 + 1
-long long c, p[M][N], dp[M][MX]; short t=0, m, n, ks, k[N], ans[M][MX]; char name[N][7];
-int info[MX][N], id[X], d[] = {1, 9, 81, 729, 6561, 59049, 531441, 4782969}, s[N];
+char stock[N][N]; int pp[N], id[X], t[MX], a[N], s[N], k[N], kase = 0, cc, m, n, K;
+double c; long long p[N][M], d[M][MX]; struct node { int x, i, f; } ans[M][MX];
 
-void print(short day, short id) {
-    if (day < 1) return;
-    int preId = ans[day][id];
-    print(day-1, preId);
-    if (preId == id) {
-        cout << "HOLD" << endl;
-    } else for (short i=0; i<n; ++i) if (info[preId][i] != info[id][i]) {
-        cout << (info[preId][i] < info[id][i] ? "BUY " : "SELL ") << name[i] << endl;
+void expand(int i, int s, int x = 0, int c = 0) {
+    if (i >= n) {
+        t[cc] = x; id[x] = cc++;
         return;
     }
+    expand(i+1, s+c, x);
+    if (c<k[i] && s+c<K) expand(i, s, x+pp[i], c+1);
 }
 
-void bfs() {
-    memset(dp, -1, sizeof(dp)); dp[0][0] = c;
-    for (short i=1; i<m; ++i) {
-        int mx = min(ks*d[n-1]+1, (k[n-1]+1)*d[n-1]);
-        for (short j=0; j<t && info[j][9] < mx; ++j) if (dp[i-1][j] >= 0) {
-            if (info[j][8] <= m-i && dp[i-1][j] > dp[i][j]) dp[i][j] = dp[i-1][j], ans[i][j] = j;
-            long long cc; int jj;
-            for (short k=0; k<n; ++k) {
-                if (info[j][k] > 0 && (cc = dp[i-1][j] + p[i][k]*s[k]) > dp[i][jj = id[info[j][9]-d[k]]])
-                    dp[i][jj] = cc, ans[i][jj] = j;
-                if (info[j][k] < ::k[k] && info[j][8] < ks && info[j][8] < m-i
-                    && (cc = dp[i-1][j] - p[i][k]*s[k]) > dp[i][jj = id[info[j][9]+d[k]]])
-                    dp[i][jj] = cc, ans[i][jj] = j;
-            }
+void print(int i, const node& t) {
+    if (i > 1) print(i-1, ans[i-1][t.x]);
+    (t.f ? cout << (t.f<0 ? "SELL " : "BUY ") << stock[t.i] : cout << "HOLD") << endl;
+}
+
+void solve() {
+    pp[n-1] = 1; for (int i=n-2; i>=0; --i) pp[i] = N*pp[i+1];
+    for (int i=0; i<n; ++i) {
+        cin >> stock[i] >> s[i] >> k[i];
+        for (int j=0; j<m; ++j) {
+            double x; cin >> x;
+            p[i][j] = 100*x + .5;
         }
     }
-    long long max = dp[m-1][0], cc; ans[m][0] = 0;
-    for (short j=1; j<t && info[j][9] <= d[n-1]; ++j) if (dp[m-1][j] >= 0 && info[j][8] == 1)
-        for (short k=0; k<n; ++k) if (info[j][k] == 1) {
-            if ((cc = dp[m-1][j] + p[m][k]*s[k]) > max) max = cc, ans[m][0] = j;
-            break;
+    expand(cc = 0, 0);
+    memset(d, -1, sizeof(d)); d[0][0] = 100*c + .5;
+    for (int i=0; i<m; ++i) for (int j=0; j<cc; ++j) if (d[i][j] >= 0) {
+        int cnt = 0;
+        for (int s=n-1, x=t[j]; s>=0; --s, x/=N) cnt += (a[s] = x%N);
+        if (d[i][j] > d[i+1][j]) d[i+1][j] = d[i][j], ans[i+1][j].x = j, ans[i+1][j].f = 0;
+        if (cnt>0) for (int c=0; c<n; ++c) if (a[c] > 0) {
+            long long v = d[i][j] + s[c] * p[c][i]; int x = id[t[j] - pp[c]];
+            if (v > d[i+1][x]) d[i+1][x] = v, ans[i+1][x].x = j, ans[i+1][x].i = c, ans[i+1][x].f = -1;
         }
-    cc = max%100;
-    cout << max/100 << (cc < 10 ? ".0" : ".") << cc << endl;
-    print(m, 0);
+        if (cnt<K) for (int c=0; c<n; ++c) if (a[c]<k[c]) {
+            long long v = d[i][j] - s[c] * p[c][i];
+            if (v < 0) continue;
+            int x = id[t[j] + pp[c]];
+            if (v > d[i+1][x]) d[i+1][x] = v, ans[i+1][x].x = j, ans[i+1][x].i = c, ans[i+1][x].f = 1;
+        }
+    }
+    if (kase++) cout << endl;
+    cout << d[m][0] / 100;
+    d[m][0] %= 100;
+    cout << (d[m][0]<10 ? ".0" : ".") << d[m][0] << endl;
+    print(m, ans[m][0]);
 }
 
 int main() {
     // freopen("in.txt", "r", stdin);
     // freopen("ou.txt", "w", stdout);
-    int kase = 0; short b[N]; double cc;
-    for (int i=0; i<X; ++i) {
-        int sum = 0, v = i;
-        for (int j=0; j<8; ++j) b[j] = v%9, sum += b[j], v /= 9;
-        if (sum <= 8) {
-            for (int j=0; j<8; ++j) info[t][j] = b[j];
-            info[t][8] = sum; info[t][9] = i; id[i] = t++;
-        }
-    }
-    while (cin >> cc >> m >> n >> ks) {
-        if (kase++ > 0) cout << endl;
-        c = cc * 100 + .5;
-        for (short i=0; i<n; ++i) {
-            cin >> name[i] >> s[i] >> k[i];
-            for (short j=1; j<=m; ++j) cin >> cc, p[j][i] = cc * 100 + .5;
-        }
-        bfs();
-    }
+    while (cin >> c >> m >> n >> K) solve();
     return 0;
 }
