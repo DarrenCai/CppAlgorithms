b29b5893f5b16cfc3715daf9e386462a3875e55b
DarrenCai <lcai13@fudan.edu.cn>
2022-10-11 22:03:38 +0800

    表1-10，UVa1379/LA3679，Pitcher Rotation，棒球投手

--- UVa/Pitcher Rotation.cpp
+++ UVa/Pitcher Rotation.cpp
@@ -1,6 +1,7 @@
 /**
- * UVa1379
+ * UVa1379/LA3679
  * 棒球投手
+ * Asia Kaohsiung 2006
  * 本题存疑，详见 https://www.cnblogs.com/jionkitten/p/12984866.html
  */
 
@@ -9,68 +10,65 @@
 using namespace std;
 
 #define N 105
-// #define M 33     // 坑啊，题目说m不超过30的
 #define G 220
-short n, m, g, p[G], dp[2][6][6][6][6];
+#define X 6         // #define X 10
+int f[G][X][X][X][X], y[G];
 struct node {
     short p, s;
     bool operator< (const node& rhs) const {
         return s > rhs.s;
     }
-} s[N][N]={0};  // s[M]][N]={0}
+} s[N][N]={0};
 
-int main()
-{
+int p(int x, int i) {
+    if (x < 0) return 0;
+    return i>0 || !y[x] ? s[y[x]][i].p : -1;
+}
+
+void solve() {
+    int n, m, g, ans = 0; cin >> n >> m >> g; g+=9;
+    for (int i=1; i<=m; ++i) {
+        for (int j=1; j<=n; ++j) cin >> s[i][j].s, s[i][j].p = j;
+        sort(s[i]+1, s[i]+1+n);
+    }
+    int t = min(X, n+1);
+    for (int x=0; x<=g; ++x) {
+        cin >> y[x];
+        for (int a=0; a<t; ++a) {
+            int pa = p(x-3, a);
+            if (pa < 0) continue;
+            for (int b=0; b<t; ++b) {
+                int pb = p(x-2, b);
+                if (pb < 0 || (pb && pb==pa)) continue;
+                for (int c=0; c<t; ++c) {
+                    int pc = p(x-1, c);
+                    if (pc < 0 || (pc && (pc==pa || pc==pb))) continue;
+                    for (int d=0; d<t; ++d) {
+                        int pd = p(x, d);
+                        if (pd < 0 || (pd && (pd==pa || pd==pb || pd==pc))) continue;
+                        int &r = f[x+1][a][b][c][d] = 0;
+                        for (int e = x>3 && y[x-4] ? 1 : 0; e<t; ++e) {
+                            int pe = x<4 ? 0 : s[y[x-4]][e].p;
+                            if (pe && (pe==pa || pe==pb || pe==pc || pe==pd)) continue;
+                            r = max(r, f[x][e][a][b][c] + s[y[x]][d].s);
+                        }
+                        if (x == g) ans = max(ans, r);
+                    }
+                }
+            }
+        }
+    }
+    cout << ans/100;
+    ans %= 100;
+    cout << (ans < 10 ? ".0" : ".") << ans << endl;
+}
+
+int main() {
     // freopen("in.txt", "r", stdin);
     // freopen("ou.txt", "w", stdout);
+    for (int a=0; a<X; ++a) for (int b=0; b<X; ++b)
+        for (int c=0; c<X; ++c) for (int d=0; d<X; ++d) f[0][a][b][c][d] = 0;
     short t; cin >> t;
-    while (t--) {
-        cin >> n >> m >> g; g+=10;
-        for (short i=1; i<=m; ++i) {
-            for (short j=1; j<=n; ++j) cin >> s[i][j].s, s[i][j].p = j;
-            sort(s[i]+1, s[i]+1+n);
-        }
-        for (short i=0; i<g; ++i) cin >> p[i];
-        short cur = 0, nex = 1, a, b, c, d, e, aa, bb, cc, dd, ee, ans = 0, r;
-        for (a = p[g-=4]==0 ? 0 : 1; aa = s[p[g]][a].p, a<6; ++a) {
-            for (b = p[g+1]==0 ? 0 : 1; bb = s[p[g+1]][b].p, b<6; ++b) if (b==0 || aa!=bb) {
-                for (c = p[g+2]==0 ? 0 : 1; cc = s[p[g+2]][c].p, c<6; ++c) if (c==0 || (aa!=cc && bb!=cc)) {
-                    for (d = p[g+3]==0 ? 0 : 1; dd = s[p[g+3]][d].p, d<6; ++d)
-                        if (d==0 || (aa!=dd && bb!=dd && cc!=dd)) {
-                            dp[cur][a][b][c][d] = s[p[g]][a].s + s[p[g+1]][b].s + s[p[g+2]][c].s + s[p[g+3]][d].s;
-                            if (d==0) break;
-                        }
-                    if (c==0) break;
-                }
-                if (b==0) break;
-            }
-            if (a==0) break;
-        }
-        while (--g >= 0) {
-            for (a = p[g]==0 ? 0 : 1; aa = s[p[g]][a].p, a<6; ++a) {
-                for (b = p[g+1]==0 ? 0 : 1; bb = s[p[g+1]][b].p, b<6; ++b) if (b==0 || aa!=bb) {
-                    for (c = p[g+2]==0 ? 0 : 1; cc = s[p[g+2]][c].p, c<6; ++c) if (c==0 || (aa!=cc && bb!=cc)) {
-                        for (d = p[g+3]==0 ? 0 : 1; dd = s[p[g+3]][d].p, d<6; ++d)
-                            if (d==0 || (aa!=dd && bb!=dd && cc!=dd)) {
-                                short &ref = dp[nex][a][b][c][d] = 0;
-                                for (e = p[g+4]==0 ? 0 : 1; ee = s[p[g+4]][e].p, e<6; ++e)
-                                    if (e==0 || (aa!=ee && bb!=ee && cc!=ee && dd!=ee)) {
-                                        ref = max(int(ref), s[p[g]][a].s + dp[cur][b][c][d][e]);
-                                        if (g==0) ans = max(ans, ref);
-                                        if (e==0) break;
-                                    }
-                                if (d==0) break;
-                            }
-                        if (c==0) break;
-                    }
-                    if (b==0) break;
-                }
-                if (a==0) break;
-            }
-            cur ^= 1; nex ^= 1;
-        }
-        r = ans % 100;
-        cout << ans/100 << (r < 10 ? ".0" : ".") << r << endl;
-    }
+    while (t--) solve();
     return 0;
 }
