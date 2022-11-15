c51535b9eea494a2ddc387544057a83617f950ae
DarrenCai <lcai13@fudan.edu.cn>
2022-11-15 14:16:18 +0800

    UVa1628/LA5847，Pizza Delivery，送匹萨

--- UVa/Pizza Delivery.cpp
+++ UVa/Pizza Delivery.cpp
@@ -1,99 +1,51 @@
 /**
- * UVa1628
+ * UVa1628/LA5847
  * 送匹萨
+ * Asia Daejeon 2011
  */
 
 #include <iostream>
-#include <map>
+#include <algorithm>
 using namespace std;
 
-#define N 100
+#define N 105
+int d[N][N][2][N], p[N], e[N], n;
 
-map<int, int> d[N][N][2]; int p[N], e[N];
+void solve() {
+    cin >> n;
+    for (int i=0; i<n; ++i) cin >> p[i];
+    for (int i=0; i<n; ++i) cin >> e[i];
+    int x = upper_bound(p, p+n, 0) - p, ans = 0;
+    if (x == 0) {
+        for (int i=0; i<n; ++i) ans += max(0, e[i] - p[i]);
+    } else if (x == n) {
+        for (int i=0; i<n; ++i) ans += max(0, e[i] + p[i]);
+    } else {
+        int y = x-1;
+        for (int i=0; i<n; ++i) for (int j=n-1; j>=i; --j) {
+            d[i][j][0][0] = d[i][j][1][0] = 0;
+            for (int k=1, a=min(x,i), b=max(y,j), t=n-b+a; k<t; ++k) {
+                d[i][j][0][k] = d[i][j][1][k] = -100000000;
+                for (int z=a-1; z>=0 && n-b+z>=k; --z) {
+                    d[i][j][0][k] = max(d[i][j][0][k], d[z][j][0][k-1] + e[z] - k*(p[i]-p[z]));
+                    d[i][j][1][k] = max(d[i][j][1][k], d[z][j][0][k-1] + e[z] - k*(p[j]-p[z]));
+                }
+                for (int z=b+1; z<n && n-z+a>=k; ++z) {
+                    d[i][j][0][k] = max(d[i][j][0][k], d[i][z][1][k-1] + e[z] - k*(p[z]-p[i]));
+                    d[i][j][1][k] = max(d[i][j][1][k], d[i][z][1][k-1] + e[z] - k*(p[z]-p[j]));
+                }
+            }
+        }
+        for (int i=x; i<n; ++i) for (int k=n-i+y+1; k>0; --k) ans = max(ans, d[i][i][0][k-1] + e[i] - k*p[i]);
+        for (int i=y; i>=0; --i) for (int k=n-x+i+1; k>0; --k) ans = max(ans, d[i][i][0][k-1] + e[i] + k*p[i]);
+    }
+    cout << ans << endl;
+}
 
-int main()
-{
+int main() {
     // freopen("in.txt", "r", stdin);
     // freopen("ou.txt", "w", stdout);
-    short n; cin >> n;
-    while (cin >> n) {
-        short x = 0; int ans = 0, m = 0;
-        for (short i=0; i<n; ++i) {
-            cin >> p[i]; if (p[x] < 0 && p[i] > 0) x = i;
-        }
-        for (short i=0; i<n; ++i) cin >> e[i], m = max(e[i], m);
-        if (x == 0) {
-            if (p[0] > 0) {
-                for (short i=0; i<n; ++i) ans += max(e[i]-p[i], 0);
-            } else {
-                for (short i=0; i<n; ++i) ans += max(e[i]+p[i], 0);
-            }
-        } else {
-            for (short i=0; i<n; ++i) for (short j=i+1; j<n; ++j) d[i][j][0].clear(), d[i][j][1].clear();
-            d[x][x][0][p[x]] = d[x][x][1][p[x]] = ans = max(e[x] - p[x], 0);
-            ans = max(ans, d[x-1][x-1][0][-p[x-1]] = d[x-1][x-1][1][-p[x-1]] = max(e[x-1] + p[x-1], 0));
-            for (short i=x-2; i>=0; --i) {
-                int t = -p[i]; if (t > m) break;
-                ans = max(ans, d[i][i][0][t] = d[i][i][1][t] = d[i+1][i+1][0][-p[i+1]] + max(e[i] + p[i], 0));
-            }
-            for (short i=x+1; i<n; ++i) {
-                int t = p[i]; if (t > m) break;
-                ans = max(ans, d[i][i][0][t] = d[i][i][1][t] = d[i-1][i-1][0][p[i-1]] + max(e[i] - p[i], 0));
-            }
-            for (short i=x-1; i>=0; --i) for (short j=x; j<n; ++j) {
-                int t = 2*p[j]-p[i], v = 0;
-                if (t <= m) {
-                    for (short k=x; k<=j; ++k) v += max(e[k]-p[k], 0);
-                    for (short k=x-1; k>=i; --k) v += max(e[k]-2*p[j]+p[k], 0);
-                    ans = max(ans, d[i][j][0][t] = v);
-                }
-                t = p[j]-2*p[i]; v = 0;
-                if (t <= m) {
-                    for (short k=x-1; k>=i; --k) v += max(e[k]+p[k], 0);
-                    for (short k=x; k<=j; ++k) v += max(e[k]-p[k]+2*p[i], 0);
-                    ans = max(ans, d[i][j][1][t] = v);
-                }
-            }
-            for (short i=x-1; i>=0; --i) for (short j=x; j<n; ++j) {
-                map<int, int>::iterator it;
-                for (it = d[i][j][0].begin(); it != d[i][j][0].end(); ++it) {
-                    if (j+1 < n) {
-                        int t = it->first + p[j+1] - p[i];
-                        if (t <= m) {
-                            int v = it->second + max(e[j+1]-t, 0); ans = max(ans, v);
-                            if (d[i][j+1][1].count(t)) d[i][j+1][1][t] = max(d[i][j+1][1][t], v);
-                            else d[i][j+1][1][t] = v;
-                        }
-                    }
-                    if (i > 0) {
-                        int t = it->first + p[i] - p[i-1];
-                        if (t <= m) {
-                            int v = it->second + max(e[i-1]-t, 0); ans = max(ans, v);
-                            if (d[i-1][j][0].count(t)) d[i-1][j][0][t] = max(d[i-1][j][0][t], v);
-                            else d[i-1][j][0][t] = v;
-                        }
-                    }
-                }
-                for (it = d[i][j][1].begin(); it != d[i][j][1].end(); ++it) {
-                    if (j+1 < n) {
-                        int t = it->first + p[j+1] - p[j];
-                        if (t <= m) {
-                            int v = it->second + max(e[j+1]-t, 0); ans = max(ans, v);
-                            if (d[i][j+1][1].count(t)) d[i][j+1][1][t] = max(d[i][j+1][1][t], v);
-                            else d[i][j+1][1][t] = v;
-                        }
-                    }
-                    if (i > 0) {
-                        int t = it->first + p[j] - p[i-1];
-                        if (t <= m) {
-                            int v = it->second + max(e[i-1]-t, 0); ans = max(ans, v);
-                            if (d[i-1][j][0].count(t)) d[i-1][j][0][t] = max(d[i-1][j][0][t], v);
-                            else d[i-1][j][0][t] = v;
-                        }
-                    }
-                }
-            }
-        }
-        cout << ans << endl;
-    }
+    short t; cin >> t;
+    while (t--) solve();
+    return 0;
 }
