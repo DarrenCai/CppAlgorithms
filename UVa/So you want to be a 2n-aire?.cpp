a4dadf0ff30b6cc4a3807187d417c7b2b115ba3a
DarrenCai <lcai13@fudan.edu.cn>
2023-03-23 13:43:13 +0800

    表2-9 概率 习题 UVa10900 你想当2^n元富翁吗?

--- UVa/So you want to be a 2n-aire?.cpp
+++ UVa/So you want to be a 2n-aire?.cpp
@@ -7,19 +7,20 @@
 #include <iomanip>
 using namespace std;
 
-int main()
-{
+double solve(int n, double t) {
+    double ans = 1.;
+    while (n--) {
+        double x = max(.5/ans, t);
+        ans = (x-t + (1-x)*(1+x)*ans) / (1-t);
+    }
+    return ans;
+}
+
+int main() {
     // freopen("in.txt", "r", stdin);
     // freopen("ou.txt", "w", stdout);
     cout << fixed << setprecision(3);
-    short n; double t;
-    while (cin>>n>>t && n) {
-        double d = 1<<n;
-        for (int i=n-1; i>=0; --i) {
-            double p = (1<<i) / d;
-            d = p<=t ? (1+t)/2*d : ((1<<i)*(p-t) + (1+p)/2*d*(1-p))/(1-t);
-        }
-        cout << d << endl;
-    }
+    int n; double t;
+    while (cin>>n>>t && n) cout << solve(n, t) << endl;
     return 0;
 }
