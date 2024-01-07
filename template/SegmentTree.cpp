e2b1adafea24e05f537d4813ce4d4ae448d90b2a
DarrenCai <lcai13@fudan.edu.cn>
2024-01-07 22:10:48 +0800

    更新模板

--- template/SegmentTree.cpp
+++ template/SegmentTree.cpp
@@ -1,17 +1,25 @@
+/**
+ * 线段树模板，很容易改成动态开点形式的
+ * 单次点修改操作涉及到的区间结点数上限是⌈logN⌉+1,
+ * 单次区间修改操作涉及到的区间结点数上限是4⌈logN⌉,
+ * 结合实际进行的不同区间操作次数上限可以得出动态结点数上限
+ * 具体可以参见Heap Manager.cpp
+ */
+
 #define N 1000100
 #define INF 1000000000
 struct node {int s, x, y; node(int s, int x, int y): s(s), x(x), y(y) {}} p(0, 0, 0);
 int sum[N], minv[N], maxv[N], add[N], set[N], y1, y2, n;
 
-node query(int i, int o, int l, int r, int t = 0) {
+node query(int o, int l, int r, int t = 0) {
     if (set[o] >= 0) {
         t += add[o] + set[o];
         return node(t * (min(r, y2) - max(l, y1) + 1), t, t);
     }
     if (l>=y1 && r<=y2) return node(sum[o] + t*(r-l+1), t + minv[o], t + maxv[o]);
     int ss = 0, sx = INF, sy = 0, m = (l+r)>>1, lc = o<<1, rc = lc+1; t += add[o];
-    if (y1 <= m) p = query(i, lc, l, m, t), ss += p.s, sx = min(sx, p.x), sy = max(sy, p.y);
-    if (y2 > m) p = query(i, rc, m+1, r, t), ss += p.s, sx = min(sx, p.x), sy = max(sy, p.y);
+    if (y1 <= m) p = query(lc, l, m, t), ss += p.s, sx = min(sx, p.x), sy = max(sy, p.y);
+    if (y2 > m) p = query(rc, m+1, r, t), ss += p.s, sx = min(sx, p.x), sy = max(sy, p.y);
     return node(ss, sx, sy);
 }
 
@@ -22,7 +30,7 @@ void pushdown(int o, int l, int r) {
         int lc = o<<1, rc = lc+1;
         sum[o] = sum[lc] + sum[rc]; minv[o] = min(minv[lc], minv[rc]); maxv[o] = max(maxv[lc], maxv[rc]);
         sum[o] += add[o]*(r-l+1); minv[o] += add[o]; maxv[o] += add[o];
-    } else sum[o] = add[o], minv[o] = add[o], maxv[o] = add[o];
+    } else sum[o] = minv[o] = maxv[o] = add[o];
 }
 
 // 这是区间修改的例子
--- template/splay(带父指针).cpp
+++ template/splay(带父指针).cpp
@@ -1,5 +1,7 @@
+#define N 100050
 struct node {
-    node *ch[2] = {NULL, NULL};     //左右子树
+    node *ch[2] = {NULL, NULL},     //左右子树
+         *fa = NULL;                //父指针
     int v;                          //值
     int s = 0;                      //附加域size
     int flip = 0;                   //翻转标记
@@ -17,12 +19,20 @@ struct node {
             ch[0]->flip ^= 1; ch[1]->flip ^= 1;
         }
     }
-} *null = new node();
+} *null = new node(), *q[N];
 
 //d=0 代表左旋，d=1 代表右旋
-void rotate(node*& o, int d) {
-    node* k = o->ch[d^1]; o->ch[d^1] = k->ch[d]; k->ch[d] = o;
-    o->maintain(); k->maintain(); o = k; //注意必须先维护o，再维护k
+node* rotate(node*& o, int d) {
+    node* k = o->ch[d^1];
+    o->ch[d^1] = k->ch[d];
+    k->ch[d]->fa = o;
+    k->ch[d] = o;
+    if (o->fa) o->fa->ch[o->fa->ch[1] == o] = k;
+    k->fa = o->fa;
+    o->fa = k;
+    o->maintain();
+    k->maintain();
+    return k;
 }
 
 void splay(node*& o, int k) {  // 找到序列的左数第k个元素并伸展到根结点
@@ -35,16 +45,17 @@ void splay(node*& o, int k) {  // 找到序列的左数第k个元素并伸展到
     int d2 = p->cmp(k);  // 第k个元素是在p的左子树?→d2
     if (d2 != -1) {          // 不是子树的根，伸展到p
         splay(p->ch[d2], d2 ? k - p->ch[0]->s - 1 : k);  // 伸展到p的子树根，下面旋转到p
-        if (d == d2) rotate(o, d^1);  // 一条直线
-        else rotate(o->ch[d], d);  // 不是一条直线
+        if (d == d2) o = rotate(o, d^1);  // 一条直线
+        else o->ch[d] = rotate(o->ch[d], d);  // 不是一条直线
     }
-    rotate(o, d^1);  // 从p旋转到o
+    o = rotate(o, d^1);  // 从p旋转到o
 }
 
 // 合并left和right。假定left的所有元素比right小。注意right可以是null，但left不可以
 node* merge(node* left, node* right) {
     splay(left, left->s);
     left->ch[1] = right;
+    right->fa = left;
     left->maintain();
     return left;
 }
@@ -54,8 +65,20 @@ void split(node* o, int k, node*& left, node*& right) {
     splay(o, k);
     left = o;
     right = o->ch[1];
+    right->fa = NULL;
     o->ch[1] = null;
     left->maintain();
+}
+
+// 查询指定结点的排名
+int rnk(node* o) {
+    int s = 0, t = 1; q[0] = o;
+    while (o->fa) o = o->fa, q[t++] = o;
+    while (t--) {
+        q[t]->pushdown();
+        if (!t || q[t-1] == q[t]->ch[1]) s += q[t]->ch[0]->s + 1;
+    }
+    return s;
 }
 
 // build(arr)：把数组arr转化为序列，返回这个序列
